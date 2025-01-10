f0590d492ec6a7be688fa9a5fc350223a2f70224
Hanzlah Javed <hanzlahjavaid849@gmail.com>
2025-01-10 20:12:36 +0500

    changed API_URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-// export const API_URL = "https://cloudbravt.centralindia.cloudapp.azure.com";
-export const API_URL = "http://localhost:4000";
+export const API_URL = "https://cloudbravt.centralindia.cloudapp.azure.com";
+// export const API_URL = "http://localhost:4000";
